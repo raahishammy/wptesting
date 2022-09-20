e70d348e0c44d637b733fdaa1d6741a511b12172
Adam <uday.jbbn@gmail.com>
2022-09-20 00:38:50 -0700

    Styles Updateds

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wptest' );
+define( 'DB_NAME', 'dapps_wptesting' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'dapps_wptesting' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'GVh+Bnzn0^7W' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
--- wp-content/themes/twentytwenty-child/assets/js/script.js
+++ wp-content/themes/twentytwenty-child/assets/js/script.js
@@ -11,7 +11,31 @@
         // prevArrow: $('.prev'),
         // nextArrow: $('.<i class="far fa-chevron-circle-right"></i>'),
         arrows: true,
-        adaptiveHeight: false
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
     });
 
     
